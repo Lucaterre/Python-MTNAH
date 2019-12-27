5ca776c011a8ce9762bc7584c983b689060271a6
Lucas Terriel <ls.terriel@gmail.com>
2019-12-27 18:11:53 +0100

    uploadedex inter pour Chap2

--- Chp2/Exinter.py
+++ Chp2/Exinter.py
@@ -16,7 +16,7 @@ fichier_ouvert.close()
 #Exercice 2
 
 """
-Code qui compte certains éléments apparaissant dans une liste. 
+Programme qui compte certains éléments apparaissant dans une liste. 
 Écrivez un bloc de code qui définit la variable nombre_de_resultats 
 et compte combien de fois le mot à (assigné à a_compter) 
 apparaît dans la liste de mots appelée mots.
@@ -62,7 +62,7 @@ print(my_dict)
 
 
 """
-Alternative :
+Alternative pour chat qui court... :
 
 phrase = "Le petit chat court dans la prairie"
 
@@ -79,4 +79,40 @@ print(resultat)
 si phrase.split() => on coupe la phrase par mot et non par char
 
 """
+
+#SERIE D'EXERCICES INTERMEDIAIRES SUR LES FONCTIONS
+
+"""
+Exercice 1 : Créer une fonction qui conjuge un verbe
+du premier groupe au présent de l'indicatif
+à la personne demandée
+
+"""
+
+def conjugue(verbe, clé):
+    """
+    Conjugue un verbe par extraction du radical et ajout de la terminaison
+    stocké dans un dictionnaire
+    :param verbe: verbe a conjuguer
+    :type verbe : CHAR
+    :param clé: clé du dictionnaire de terminaisons
+    :type clé : INT
+    :returns : verbe(VAR verbe) conjuguer à la personne demandé(INT clé)
+    :rtype : CHAR
+    :
+    """
+    terminaisons = {2: "es", 5: "ez", 3: "ons"}
+    radical = verbe[:-2]
+    verbe_conj = radical + terminaisons[clé]
+    return  verbe_conj
+
+
+print(conjugue("manger", 2))
+print(conjugue("balayer", 5))
+print(conjugue("travailler", 3))
+
+# Vérification
+assert conjugue("manger", 2) == "manges"
+assert conjugue("balayer", 5) == "balayez"
+assert conjugue("travailler", 3) == "travaillons"
 
